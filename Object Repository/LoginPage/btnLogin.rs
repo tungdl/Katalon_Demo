2e354d95b411021542cc8725efc58ef8ba8a4b52
tungdl <tungdl@vnpt.vn>
2020-03-10 16:16:04 +0700

    Initial commit

---
+++ Object Repository/LoginPage/btnLogin.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btnLogin</name>
+   <tag></tag>
+   <elementGuidId>95cdf29f-5824-40df-ab16-43f0a3576aed</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
